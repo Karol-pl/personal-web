ff5a931f8fffad80fb50c24f55d8df27c0d60615
Karol Homziuk <karolhomziuk@gmail.com>
2024-09-29 10:41:10 -0300

    nav fixes

---
+++ src/scripts/active.ts
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+const config = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.7,
+};
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      ioHandler(entry);
+    }
+  });
+}, config);
+
+sections.forEach((section) => observer.observe(section));
+
+const ioHandler = (entry: IntersectionObserverEntry) => {
+  const id = entry.target.id;
+
+  navLinks.forEach((navlink) => {
+    navlink.classList.remove("active");
+    if (navlink.getAttribute("href") === `/#${id}`) {
+      navlink.classList.add("active");
+    }
+  });
+};
