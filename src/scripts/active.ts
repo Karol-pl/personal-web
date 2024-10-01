61b54add051fd1083b068bdbffe3bd56d0cf37d1
Karol Homziuk <karolhomziuk@gmail.com>
2024-10-01 20:14:47 -0300

    fixes

--- src/scripts/active.ts
+++ src/scripts/active.ts
@@ -1,28 +1,32 @@
 const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll("nav a");
+const links = document.querySelectorAll("[data-after]");
 
 const config = {
   root: null,
-  rootMargin: "-30px 0px -60%",
-  threshold: 1,
+  rootMargin: "-30px 0px -40%",
+  threshold: 0.5,
 };
 
 let observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
+      console.log(`#${entry.target.id} is intersecting`);
       ioHandler(entry);
     }
   });
 }, config);
 
+console.log(observer);
+
 sections.forEach((section) => observer.observe(section));
 
 const ioHandler = (entry: IntersectionObserverEntry) => {
   const id = entry.target.id;
+  console.log("entry id", id);
 
-  navLinks.forEach((navlink) => {
+  links.forEach((navlink) => {
     navlink.classList.remove("active");
-    if (navlink.getAttribute("href") === `/#${id}`) {
+    if (navlink.getAttribute("href") === `#${id}`) {
       navlink.classList.add("active");
     }
   });
