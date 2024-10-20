b313e5531739b880cd41e0fd4e1b08cbdd809981
Karol Homziuk <karolhomziuk@gmail.com>
2024-10-20 19:07:21 -0300

    simplistic ui v1.0.0

--- src/scripts/active.ts
+++ src/scripts/active.ts
@@ -1,36 +1,5 @@
-// Active navigation links
-const sections = document.querySelectorAll("section");
-const links = document.querySelectorAll("[data-after]");
-
-const config = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.75,
-};
-
-let observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      ioHandler(entry);
-    }
-  });
-}, config);
-
-sections.forEach((section) => observer.observe(section));
-
-const ioHandler = (entry: IntersectionObserverEntry) => {
-  const id = entry.target.id;
-
-  links.forEach((navlink) => {
-    navlink.classList.remove("active");
-    if (navlink.getAttribute("href") === `#${id}`) {
-      navlink.classList.add("active");
-    }
-  });
-};
-
 // Active gallery images
-const galleryImages = document.querySelectorAll(".about-gallery img");
+const galleryImages = document.querySelectorAll(".gallery img");
 
 galleryImages.forEach((image) => {
   image.addEventListener("click", () => {
