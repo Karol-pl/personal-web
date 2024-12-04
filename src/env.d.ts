6a3a7b9a16e775ff631bb76bdf0a9ff1209251e2
Karol Homziuk <karolhomziuk@gmail.com>
2024-12-04 12:21:49 -0300

    set up labs (#6)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface importMetaEnv {
+  NASA_API_KEY: string;
+  NASA_DEMO_KEY: string;
+  CRON_URL: string;
+  GREETING: string;
+}
+
+interface ImportMeta {
+  readonly env: importMetaEnv;
+}
